b23c831a15998ff0c0763eae483c43bc25deefc1
Ori Nachum <ori.nachum@gmail.com>
2024-03-22 17:46:12 +0000

    exporting anthropic client to file

---
+++ modelproviders/anthropic_client.py
@@ -0,0 +1,66 @@
+import requests
+import os
+import datetime
+from dotenv import load_dotenv
+import anthropic
+
+load_dotenv()  # Load environment variables from .env file
+
+API_KEY = os.getenv("ANTHROPIC_API_KEY")
+API_URL = "https://api.anthropic.com/v1/complete"
+CHAT_API_URL = "https://api.anthropic.com/v1/chat/completions"
+HISTORY_FILE = "conversation_history.txt"
+SYSTEM_PROMPT_FILE = "prompts/system.md"
+
+if not API_KEY:
+    raise ValueError("ANTHROPIC_API_KEY environment variable is not set.")
+
+client = anthropic.Anthropic()
+
+def get_model_id_by_name(model):
+  if model == "opus":
+    return "claude-3-opus-20240229"
+  if model == "haiku":
+    return "claude-3-haiku-20240307"
+  if model == "sonnet":
+    return "claude-3-sonnet-20240229"
+
+def generate_response(prompt, history, system_prompt, client, model):
+    model_id = get_model_id_by_name(model)
+    messages = []
+    if history:
+        for entry in history.split("\n"):
+            if "[User]" in entry:
+                user_message = entry.split("[User]: ")[1]
+                messages.append({"role": "user", "content": user_message})
+            elif "[Assistant]" in entry:
+                assistant_message = entry.split("[Assistant]: ")[1]
+                messages.append({"role": "assistant", "content": assistant_message})
+    messages.append({"role": "user", "content": prompt})
+
+    data = {
+        "messages": messages,
+        "max_tokens_to_sample": 1000,
+        "stop_sequences": []
+    }
+
+    message = client.messages.create(
+        model = model_id,
+        messages = messages,
+        max_tokens = 1000,
+        system = system_prompt
+    )
+    content_block = message.content
+    content = content_block[0].text
+    return content
+
+
+if __name__ == "__main__":
+    history = ""
+    system_prompt = "testing client"
+
+    prompt = input("Make a request")
+
+    model = "haiku"
+    response = generate_response(prompt, history, system_prompt, client, model)
+    print(response)
