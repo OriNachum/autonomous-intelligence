b2fe0fdee84964803db3176dd839be365d439ed3
Ori Nachum <ori.nachum@gmail.com>
2024-03-28 23:25:18 +0000

    feat(tau): prepare file to extract model actions

---
+++ services/extract_actions.py
@@ -0,0 +1,13 @@
+import re
+
+def extract_actions(text):
+    # Extract from the text only * asteriks. Drop to double new line between quotes.
+    # Use regex to pull all text within asteriks
+    text = re.findall(r'\*(.+?)\*', text)
+    text = "\n".join(text)
+    print (text)
+
+if __name__ == "__main__":
+    path = extract_actions("Hello, World! \"This is a test\". *Tapping my hat* This should not be spoken. \"This is another test\". *Taking a picture* And here is another test.")
+    # Play the audio file, not using ffplay
+    
