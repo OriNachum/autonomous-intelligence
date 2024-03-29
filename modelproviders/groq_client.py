8f591be5cfeb97ae8019139cefb54ad95285dda8
Ori Nachum <ori.nachum@gmail.com>
2024-03-29 11:22:26 +0000

    feat(tau): improved groq client

--- modelproviders/groq_client.py
+++ modelproviders/groq_client.py
@@ -5,18 +5,35 @@ from groq import Groq
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
-def groq_completion(text):
+
+def get_model_id_by_name(model):
+  if model == "mixtral":
+    return "mixtral-8x7b-32768"
+  if model == "haiku":
+    return "claude-3-haiku-20240307"
+  if model == "sonnet":
+    return "claude-3-sonnet-20240229"
+
+
+
+def groq_completion(text, system_prompt, model):
+    model_id = get_model_id_by_name(model)
+    messages = []
+    if (system_prompt):
+        messages.append({
+            "role": "system",
+            "content": system_prompt
+        })
+    messages.append({
+        "role": "user",
+        "content": text
+    })
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": text,
-            }
-        ],
-        model="mixtral-8x7b-32768",
+        messages=messages,
+        model=model_id,
     )
     return chat_completion.choices[0].message.content
 
 if "__main__" == __name__:
-  response = groq_completion("say hello world")
+  response = groq_completion("say hello world", "You only respond in UPPERCASE", "mixtral")
   print(response)
