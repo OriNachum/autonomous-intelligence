1987ffa817e26baa55e6b90d348b7080feb4b8b3
Ori Nachum <ori.nachum@gmail.com>
2024-04-02 02:15:18 +0300

    feat(tau): add os support

--- services/nmap_scanner.py
+++ services/nmap_scanner.py
@@ -1,17 +1,37 @@
 import nmap
 
+# Initialize Nmap PortScanner
 nm = nmap.PortScanner()
 
+# Define the network range you want to scan
 network = '192.168.1.0/24'
 
-nm.scan(hosts=network, arguments='-sn')
+# Perform a scan with OS and service version detection
+# -O: Enable OS detection
+# -sV: Probe open ports to determine service/version info
+# --host-timeout: Times out if scanning a single host takes longer than specified (optional, for speed-up)
+nm.scan(hosts=network, arguments='-O -sV --host-timeout 1m')
 
-# Print the devices found
-print("Available devices o nthe network:")
-print("IP\t\tMAC Address")
-for host in nm.all_hosts(): 
-    if 'mac' in nm[host]['addresses']:
-        print(f"{host}\t{nm[host]['adresses']['mac']}")
+for host in nm.all_hosts():
+    print(f"Host : {host} ({nm[host].hostname()})")
+    print(f"State : {nm[host].state()}")
+
+    for proto in nm[host].all_protocols():
+        print(f"----------")
+        print(f"Protocol : {proto}")
+
+        lport = nm[host][proto].keys()
+        sorted(lport)
+        for port in lport:
+            print(f"port : {port}\tstate : {nm[host][proto][port]['state']}, service: {nm[host][proto][port]['name']}")
+
+    # OS detection
+    if 'osclass' in nm[host]:
+        for osclass in nm[host]['osclass']:
+            print('OS Details:')
+            print(f"Type : {osclass['type']}, Vendor : {osclass['vendor']}, OS Family : {osclass['osfamily']}, OS Gen : {osclass['osgen']}")
+            print(f"Accuracy : {osclass['accuracy']}%")
     else:
-        print(f"{host}\tMAC Address Unavailable")
+        print("OS Details: Not available")
 
+    print("----------\n")
