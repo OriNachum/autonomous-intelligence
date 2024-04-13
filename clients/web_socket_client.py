d35d680d19747b6fd4fd0d91fb312e70b8cdc586
Ori Nachum <ori.nachum@gmail.com>
2024-04-13 19:55:30 +0100

    feat(communication): improved bi directional communication.

--- clients/web_socket_client.py
+++ clients/web_socket_client.py
@@ -6,7 +6,7 @@ class WebSocketClient:
     def __init__(self, vision_server_ip_location="./batch/output/nmap_output.txt"):
         self.vision_server_ip_location = vision_server_ip_location
         self.base_ip = self.locate_server()
-        SECRET_KEY="your_secret_key"
+        self.SECRET_KEY="your_secret_key"
 
     def locate_server(self):
         # Open the file in read mode
@@ -22,14 +22,19 @@ class WebSocketClient:
         uri = f"ws://{self.base_ip}:8765"
         print(uri)
         async with websockets.connect(uri) as websocket:
-            await websocket.send("{SECRET_KEY} Hello, server!")
+            messages = [f"{self.SECRET_KEY}Client pi:  Hello, server! #1", f"{self.SECRET_KEY}Client pi:  Hello, server! #2", f"{self.SECRET_KEY}Client pi:  Hello, server! #3"]
+            for message in messages:
+                await websocket.send(message)
+                await asyncio.sleep(2)
             
             async for message in websocket:
                 print(f"Received: {message}")
     
     def start_client(self):
+        print("starting")
         asyncio.get_event_loop().run_until_complete(self.setup_websocket_client())
         asyncio.get_event_loop().run_forever()
+        print("running")
             
 if __name__ == "__main__":
     # Test the FaceDetector class
