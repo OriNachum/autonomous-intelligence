05d4a8efacac8187c843e2843206fea3bd33f9ac
Ori Nachum <ori.nachum@gmail.com>
2024-04-29 13:33:23 +0100

    feat(face): elliptic colorful eyes

--- services/face_service.py
+++ services/face_service.py
@@ -13,14 +13,23 @@ pygame.display.set_caption("Dynamic Face")
 # Colros
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 
 # Face settings
 face_center = (screen_width // 2, screen_height // 2)
 face_radius = 100
 
 # Eye settings
-eye_radius = 15
-eye_offset_x, eye_offset_y = 30, 40
+eye_offset_x, eye_offset_y = 40, 40
+
+eyes = {
+    'happy': { 'size': (40, 30), 'color': GREEN },
+    'normal': { 'size': (30, 20), 'color': BLUE },
+    'sad': { 'size': (30, 15), 'color': RED }
+}
 
 # Mouth settings
 #mouth_happy = { 'width': 100, 'height': 40, 'start_angle': 160, 'stop_angle': 20}
@@ -41,10 +50,18 @@ def draw_face(expression):
     pygame.draw.circle(screen, BLACK, face_center, face_radius, 2)
 
     # Draw eyes
-    left_eye_center = (face_center[0] - eye_offset_x, face_center[1] - eye_offset_y)
-    right_eye_center = (face_center[0] + eye_offset_x, face_center[1] - eye_offset_y)
-    pygame.draw.circle(screen, BLACK, left_eye_center, eye_radius)
-    pygame.draw.circle(screen, BLACK, right_eye_center, eye_radius)
+    if expression in eyes.keys():
+        eye = eyes[expression]
+        eye_size = eye['size']
+        eye_color = eye['color']
+        left_eye_rect = [face_center[0] - eye_offset_x - eye_size[0]//2, face_center[1] - eye_offset_y, *eye_size]
+        right_eye_rect = [face_center[0] + eye_offset_x - eye_size[0]//2, face_center[1] - eye_offset_y, *eye_size]
+        pygame.draw.ellipse(screen, eye_color, left_eye_rect)
+        pygame.draw.ellipse(screen, eye_color, right_eye_rect)
+    else:
+        eye = None
+
+
 
     # Draw mouth
     if expression in mouths.keys():
