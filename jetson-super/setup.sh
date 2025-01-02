67515acbbb61a8c855d9443fc3342181c279e888
ori.nachum <ori.nachum@gmail.com>
2025-01-03 01:47:43 +0200

    chore(jetson): setup script

---
+++ jetson-super/setup.sh
@@ -0,0 +1,16 @@
+
+wget https://developer.download.nvidia.com/compute/redist/jp/v61/pytorch/torch-2.5.0a0+872d972e41.nv24.08.17622132-cp310-cp310-linux_aarch64.whl
+pip3 install torch-2.5.0a0+872d972e41.nv24.08.17622132-cp310-cp310-linux_aarch64.whl 
+
+echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+
+sudo ln -sf /usr/local/cuda/lib64/libcusparse.so.12 /usr/local/cuda/lib64/libcusparse.so
+
+wget https://developer.download.nvidia.com/compute/cusparselt/0.6.3/local_installers/cusparselt-local-tegra-repo-ubuntu2204-0.6.3_1.0-1_arm64.deb
+sudo dpkg -i cusparselt-local-tegra-repo-ubuntu2204-0.6.3_1.0-1_arm64.deb
+#sudo apt-get -y install libcusparselt0 libcusparselt-dev
+
+sudo apt-get install portaudio19-dev python-all-dev python3-all-dev
+pip install pyaudio
