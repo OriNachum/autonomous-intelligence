ddb180ed9e015de89cf29fd551a632c90e7cdd3a
Ori Nachum <ori.nachum@tipalti.com>
2025-01-03 02:05:46 +0200

    feat(baby-tau): add espeak sync

--- baby-tau/speak.py
+++ baby-tau/speak.py
@@ -2,15 +2,16 @@ import subprocess
 import threading   
 
 def speak(text):
+    try:
+        # Run espeakng with the specified language and text
+        subprocess.run(["espeak", "-v", "en-us", text], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except Exception as e:
+        print(f"Error speaking: {e}")
+
+
+def speak_async(text):
     # Use a new thread to run espeakng in the background
-    def espeak_thread():
-        try:
-            # Run espeakng with the specified language and text
-            subprocess.run(["espeak", "-v", "en-us", 
-text], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        except Exception as e:
-            print(f"Error speaking: {e}")
 
     # Create a new thread to run espeakng
-    threading.Thread(target=espeak_thread).start()
+    threading.Thread(target=speak, args=(text,)).start()
 
